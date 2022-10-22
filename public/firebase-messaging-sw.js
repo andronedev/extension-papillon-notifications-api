ce4aa3f4663d450465c8774377612c55053b1830
andronedev <andronedev@gmail.com>
2022-10-22 17:29:55 +0200

    ajout logo + handle click

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -18,3 +18,25 @@ const firebaseApp = firebase.initializeApp({
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = firebase.messaging();
+
+// This background handler is called when the app is closed or not in the foreground.
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.data.body,
+        icon: './icon.ico'
+    };
+
+    self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
+
+// on notification click
+self.addEventListener('notificationclick', function (event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('https://v3.pronote.plus')
+    );
+})
